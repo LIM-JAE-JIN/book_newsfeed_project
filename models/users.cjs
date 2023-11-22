3c27af4e08e5b6cdeda8818438cc260015dffa5b
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2023-11-22 15:05:10 +0900

    update model
    update model

--- models/users.cjs
+++ models/users.cjs
@@ -1,6 +1,7 @@
 'use strict';
 const bcrypt = require('bcrypt');
 const { Model } = require('sequelize');
+require('dotenv').config();
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
     /**
@@ -31,7 +32,10 @@ module.exports = (sequelize, DataTypes) => {
   );
 
   Users.beforeCreate(async (user) => {
-    const hashedPasswd = await bcrypt.hash(user.password, SALT_ROUND_KEY);
+    const hashedPasswd = await bcrypt.hash(
+      user.password,
+      parseInt(process.env.SALT_ROUND_KEY),
+    );
     user.password = hashedPasswd;
   });
   return Users;
