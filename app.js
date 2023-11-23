865b58173f014415c315f8d54a9e4af04fbedc17
JAEJIN <wowls9416@naver.com>
2023-11-23 16:48:49 +0900

    수정

--- app.js
+++ app.js
@@ -11,21 +11,6 @@ app.use(cookieParser());
 app.use('/api', [authRouter, profileRouter]);
 
 app.use(express.static("./assets"));
-app.get("/", (req, res) => {
-  try {
-    res.json({
-      success: true,
-      message: "api 가져오기 성공"
-    });
-  } catch (error) {
-    console.log('errorMessage', error);
-    res.status(500).json({
-      success: false,
-      message: "실패"
-    })
-  }
-})
-
 
 app.listen(SERVER_PORT, () => {
   console.log(SERVER_PORT, '포트 연결 성공');
